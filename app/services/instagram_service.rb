0879196d4bb8a233daf7234de46aff83a03fcbb3
fahad ishfaq <fahadishfaq18@gmail.com>
2020-10-01 19:37:04 +0500

    minor fix

--- app/services/instagram_service.rb
+++ app/services/instagram_service.rb
@@ -9,13 +9,8 @@ class InstagramService
       'X-Requested-With' => 'XMLHttpRequest',
       'User-Agent' => "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0"
     }
-    http = Net::HTTP.new(uri.hostname, uri.port)
-    puts "===============================http:================#{http}"
-    http.use_ssl = true
-    response = http.get(uri, headers)
-    puts "============================================response:============================#{response}"
+    response = HTTParty.get(uri)
     instagram_tag = response.body
-    puts "=======================================================================#{instagram_tag.inspect}"
     hashtag_json = JSON.parse(instagram_tag)
     hashtag_info = hashtag_json['graphql']['hashtag']
     edges = hashtag_info["edge_hashtag_to_media"]['edges']
